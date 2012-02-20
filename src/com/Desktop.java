e330642869652392eaaa829e18a3624b4b29cfc9
tasillo <tasillo@gmail.com>
2012-02-20 21:37:49 +0000

    Camera moves the size of a tile

--- src/com/Desktop.java
+++ src/com/Desktop.java
@@ -5,5 +5,6 @@ import com.badlogic.gdx.backends.jogl.JoglApplication;
 public class Desktop {
 	public static void main (String[] args) {
         new JoglApplication(new Cenital(), "Cenital", 480, 320, false);
+		//new JoglApplication(new OrthographicCameraController(), "2D Camera", 480, 320, false);
 	}
 }
